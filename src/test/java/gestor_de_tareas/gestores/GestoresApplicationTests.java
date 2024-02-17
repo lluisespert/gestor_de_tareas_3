4e7c809ea40e9c234f743e22dc7abe156706754a
lluisespert <lluisespertc@gmail.com>
2024-02-17 19:23:55 +0100

    Add files via upload
    Versión 0.1

---
+++ src/test/java/gestor_de_tareas/gestores/GestoresApplicationTests.java
@@ -0,0 +1,13 @@
+package gestor_de_tareas.gestores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
